0993657b4e1c6a567b885ce60488aa41aa03f93b
Maryna11255 <10mte94857yz>
2016-05-25 03:12:18 +0300

    first adding

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('message', function(msg){
+        io.emit('message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+    socket.broadcast.emit('hi');
+});
